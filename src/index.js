0d758b1cda4f272b3dd9fe5e311f50a3f163d0ea
Rajat <rs20021023@gmail.com>
2022-02-12 22:24:20 +0530

    css completed javascript started

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+import makeTodo from './modules/MakeTodo'
 import './style.css'
+
+const newTodoBtn =  document.querySelector(".new-todo-btn")
+
+
+const  callMakeTodo = ()=>{
+    makeTodo()
+}
+
+
+newTodoBtn.addEventListener("click",callMakeTodo)
