9fac03e4be5c4cbd6400b6c96b83910690bdfd2d
Rajat <rs20021023@gmail.com>
2022-02-17 17:45:26 +0530

    delete todo code refactored

--- src/modules/deleteTodo.js
+++ src/modules/deleteTodo.js
@@ -3,20 +3,16 @@ function deleteTodo(arr) {
   const titleLabel = document.querySelectorAll(".todo-title");
 
   const handleDeleteIcon = (e) => {
-    let elementId =
-      e.target.parentNode.previousElementSibling.children[0].children[0].getAttribute(
-        "id"
-      ); //gives id of clicked todo
-
-    arr.splice(elementId);
-    e.target.parentNode.previousElementSibling.parentNode.remove();
+    let element =
+      e.target.parentNode.previousElementSibling.children[0].children[0];
+    arr.splice(element);
+    element.remove();
   };
 
   const handleCheck = (e) => {
-    let elementId = e.target.parentNode.parentNode.parentNode; //gives id of clicked todo
-
-    arr.splice(elementId);
-    e.target.parentNode.parentNode.parentNode.remove();
+    let element = e.target.parentNode.parentNode.parentNode; //gives id of clicked todo
+    arr.splice(element);
+    element.remove();
   };
 
   deleteTodoIcon.forEach((item) => {
